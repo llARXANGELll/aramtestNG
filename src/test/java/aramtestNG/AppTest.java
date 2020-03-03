a5a46b9d1f74d80b42b21747ebb152879d549318
Хачатурян Арам Альбертович <khachaturyan.Ar.Al@sberbank.ru>
2020-03-03 18:37:22 +0300

    Подкоректировал

--- src/test/java/aramtestNG/AppTest.java
+++ src/test/java/aramtestNG/AppTest.java
@@ -1,5 +1,7 @@
 package aramtestNG;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
 import static org.junit.Assert.assertTrue;
@@ -7,8 +9,10 @@ import static org.junit.Assert.assertTrue;
 
 public class AppTest {
 
+    private static final Logger LOG = LoggerFactory.getLogger(AppTest.class);
+
     @Test
     public void simlTest1(){
-
+        LOG.info("Op");
     }
 }
