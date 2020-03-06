538c9411daabc8c07208f8b4f75126afca7a5ff8
Хачатурян Арам Альбертович <khachaturyan.Ar.Al@sberbank.ru>
2020-03-06 19:45:16 +0300

    Отработал параметрезированный тест

--- src/test/java/aramtestNG/ParametrTest.java
+++ src/test/java/aramtestNG/ParametrTest.java
@@ -6,14 +6,14 @@ import org.testng.annotations.Test;
 public class ParametrTest {
 
 
-    @DataProvider(name = "Дата провайдер")
+    @DataProvider(name = "Провайдер")
     public Object[][] dataProveder(){
         return new Object[][] {
                 {"Ivan", 35},
                 {"Vova", 44},
         };
     }
-    @Test(dataProvider = "Дата провайдер")
+    @Test(dataProvider = "Провайдер")
     public void paramsTest1(String name, int age) {
         String print = String.format("Имя: %s | Вззраст: %d", name, age);
         System.out.println(print);
